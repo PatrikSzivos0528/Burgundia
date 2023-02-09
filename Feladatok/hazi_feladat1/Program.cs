010cc617b2ab52d9f32c20dffc7f92f675fed1f5
PatrikSzivos0528 <szivospatrikdavid@gmail.com>
2023-02-09 08:51:51 +0100

    main

---
+++ Feladatok/hazi_feladat1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamok_bekerdezese
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string oszthatoe = "";
+            Console.Write("Írj be egy számmot: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 3 == 0)
+                oszthatoe += "3-al";
+            if (a % 4 == 0)
+                oszthatoe += "4-el";
+            if (a % 9 == 0)
+                oszthatoe += "9-el";
+            Console.WriteLine($"A szám osztható{oszthatoe}!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Feladatok/hazi_feladat4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viz_goz_jeg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Írd be a víz hőmérsékletét: ");
+            double homerseklet = Convert.ToDouble(Console.ReadLine());
+            if (homerseklet <= 0)
+            {
+                Console.WriteLine("A víz halmazállapota: jég.");
+            }
+            else if (homerseklet > 0 && homerseklet < 100)
+            {
+                Console.WriteLine("A víz halmazállapota: folyékony.");
+            }
+            else if (homerseklet >= 100)
+            {
+                Console.WriteLine("A víz halmazállapota: gáz.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
