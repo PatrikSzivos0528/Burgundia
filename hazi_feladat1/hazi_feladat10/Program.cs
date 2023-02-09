52233f3a7fb9e257b5330d5a3abb6ed4b05016f1
PatrikSzivos0528 <szivospatrikdavid@gmail.com>
2023-02-09 10:50:44 +0100

    main

---
+++ hazi_feladat1/hazi_feladat10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hazi_feladat10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg az első számot: ");
+            int firstnum = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Add meg a második számot: ");
+            int secondnum = Int32.Parse(Console.ReadLine());
+
+
+            Random random = new Random();
+            int randomNumber = random.Next(firstnum, secondnum);
+
+            Int32 loop = 0;
+            do
+            {
+                loop++;
+            }
+            while (loop > 4);
+
+
+        }
+    }
+}
---
+++ hazi_feladat1/hazi_feladat12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hazi_feladat12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg a dinnye átmérőjét:  ");
+            int atm = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add meg mennyi dinnye van: ");
+            int db = Convert.ToInt32(Console.ReadLine());
+
+            float n = ((atm * 2 + 60)/100);
+            float s = (float)Convert.ToDouble(n);
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+    }
+}
