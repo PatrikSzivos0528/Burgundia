1e28d7e34fd5b351971dcf59114e385a6297e5b8
PatrikSzivos0528 <112941376+PatrikSzivos0528@users.noreply.github.com>
2023-02-12 13:14:13 -0800

    Add files via upload
    Signed-off-by: PatrikSzivos0528 <112941376+PatrikSzivos0528@users.noreply.github.com>

--- Feladatok/hazi_feladat17/Program.cs
+++ Feladatok/hazi_feladat17/Program.cs
@@ -1,43 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace hazi_feladat17
+class Program
 {
-    internal class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            double a, b, c;
-
-            Console.WriteLine("   2\na x + b x + c = 0 egyenlet gyökeinek meghatározása\n");
-            Console.Write("a: ");
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.Write("b: ");
-            b = Convert.ToDouble(Console.ReadLine());
-            Console.Write("c: ");
-            c = Convert.ToDouble(Console.ReadLine());
-            double d = Math.Pow(b, 2) - 4 * a * c, gyök1, gyök2;
-            if (d < 0)
-                Console.WriteLine("Nincs valós gyök.");
-            else
-            {
-                if (d == 0)
-                {
-                    gyök1 = -b / (2 * a);
-                    Console.WriteLine("Egy valós gyök van, értéke: {0}", gyök1);
-                }
-                else
-                {
-                    gyök1 = (-b + Math.Sqrt(d)) / (2 * a);
-                    gyök2 = (-b - Math.Sqrt(d)) / (2 * a);
-                    Console.WriteLine("Két valós gyök van, értékük: {0} és {1}", gyök1, gyök2);
-                }
-            }
-            Console.Write("A kilépéshez üss le egy billentyűt!");
-            Console.ReadKey();
-        }
+        Console.Write("Elso idopont: ");
+        TimeSpan time1 = TimeSpan.Parse(Console.ReadLine());
+        Console.Write("Masodik idopont: ");
+        TimeSpan time2 = TimeSpan.Parse(Console.ReadLine());
+        Console.WriteLine($"A kulonbseg: {-(time1 - time2).Seconds} masodperc");
+        Console.ReadKey();
     }
 }
---
+++ Feladatok/hazi_feladat18/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("c: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        double vegeredmeny1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / 2 * a;
+        double vegeredmeny2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / 2 * a;
+        Console.WriteLine($"A két valós gyöke: x1 = {vegeredmeny1}, x2 = {vegeredmeny2}");
+        Console.ReadKey();
+    }
+}
