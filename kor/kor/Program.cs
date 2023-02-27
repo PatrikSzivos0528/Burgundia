47ee29a75157b5425ba99425b0e2d6f150ddef24
PatrikSzivos0528 <szivospatrikdavid@gmail.com>
2023-02-27 17:33:39 +0100

    main

--- kor/kor/Program.cs
+++ kor/kor/Program.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace kor
+
+namespace Kor
 {
     class Kor
     {
@@ -39,28 +40,56 @@ namespace kor
 
 
     }
+
+    class Henger : Kor
+    {
+        private int m;
+        private double terfogat;
+        private double felszín;
+
+        public Henger() : base () { }
+
+        public Henger( int r, int m,) : base(r) 
+        {
+            this.m = m;
+        }
+
+        public void setM(int m) { this.m = m; }
+
+        public void setKor()
+        {
+            base.setKerulet();
+            base.setTerulet();
+        }
+
+        public void setTerfogat()
+        {
+            this.terfogat = base.getTerulet() * this.m;
+        }
+
+        public void setFelszín()
+        {
+            this.felszín = base.getKerulet() * this.m + 2 * base.getTerulet();
+        }
+
+        public double getTerfogat() {  return this.terfogat; }
+        public double getFelszín() { return this.felszín; }
+    }
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Kor kor = new Kor();
-            kor.setR(3);
+        { 
 
-            
-            kor.setTerulet();
-            KiirTerulet((double)kor.getTerulet());
-            kor.setKerulet();
-            KiirKerulet((double)kor.setKerulet());
+            Console.WriteLine();
+            Console.WriteLine("Henger példa");
+            Henger h1 = new Henger(2, 5);
+            h1.setKor();
+            h1.setTerfogat();
+            h1.setFelszín();
+
+            Console.WriteLine($"Felszín: {h1.getFelszín()}, Terfogat: {h1.getTerfogat()}");
 
             Console.ReadKey();
-        }
-        static private void KiirTerulet(double r)
-        {
-            Console.WriteLine($"A kör terulete={r}");
-        }
-        static private void KiirKerulet(double r)
-        {
-            Console.WriteLine($"A kör kerulete={r}");   
         }
 
     }
--- negyszog/negyszog/negyszog/Program.cs
+++ negyszog/negyszog/negyszog/Program.cs
@@ -2,21 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace negyszog
 {
-    class negyszog
+    class Negyszog
     {
         private int a;
         private int b;
         private int T;
         private int K;
 
-        public negyszog() { }
+        public Negyszog() { }
 
-        public negyszog(int a, int b)
+        public Negyszog(int a, int b)
         {
             this.a = a;
             this.b = b;
@@ -65,16 +66,58 @@ namespace negyszog
 
     }
 
+    class Hasab : Negyszog
+    {
+        private int m;
+        private int b;
+        private double terfogat;
+        private double kerulet;
+
+
+        public Hasab() : base () { }
+
+        public Hasab( int a ,int m, int b) : base (a, b)
+        {
+            this.m = m;
+        }
+
+        public void setM(int m) { this.m = m;}
+
+        public void setNegyszog()
+        {
+            base.setkerulet();
+            base.setTerulet();
+        }
+
+        public void setTerfogat()
+        {
+            this.terfogat = base.getTerulet() * this.m;
+        }
+
+        public void setFelszín()
+        {
+            this.fekszín = base.getTerulet() * this.m + 2 * this.getTerulet ();
+        }
+
+        public double getTerfogat() { return this.terfogat; }
+        public double getFelszín() { return this.felszín; }
+
+    }
+
     internal class Program
     {
+        enum nyomtat { oldal, kerter, osszes }
+
         static void Main(string[] args)
         {
-            negyszog n1 = new negyszog();
+            Console.WriteLine();
+            Console.WriteLine("Hasab példa");
+            Hasab h1 = new Hasab(3, 6, 9);
+            h1.setNegyszog();
+            h1.setTerfogat();
+            h1.setFelszín();
 
-            Console.Write($"Az n1 négyszög oldalai: a = { n1.getA()}, b = { n1.getB()}");
-
-            n1.setA(5);
-            n1.setB(6);
+            Console.WriteLine($"Felszín: {h1.getFelszín()}, Térfogat: {h1.getTerfogat()}");
 
             Console.ReadKey();
         }
