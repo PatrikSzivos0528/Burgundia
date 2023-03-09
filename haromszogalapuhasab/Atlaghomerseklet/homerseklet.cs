99f2cc2697cb8f00fb06df894a367a8c6c755428
PatrikSzivos0528 <szivospatrikdavid@gmail.com>
2023-03-09 07:53:09 +0100

    main

--- haromszogalapuhasab/Atlaghomerseklet/homerseklet.cs
+++ haromszogalapuhasab/Atlaghomerseklet/homerseklet.cs
@@ -76,5 +76,25 @@ namespace Atlaghomerseklet
                 }
             }
         }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                TemperatureMatrix temperatureMatrix = new TemperatureMatrix();
+                temperatureMatrix.GenerateRandomTemperatures();
+                temperatureMatrix.CalculateMonthlyAverages();
+
+                Console.WriteLine("A 12 hónap átlaghőmérsékletei:");
+                for (int i = 0; i < 12; i++)
+                {
+                    Console.WriteLine("{0}. hónap: {1:0.00} fok",
+                        i + 1,
+                        temperatureMatrix.MonthlyAverages[i]);
+                }
+                Console.ReadLine();
+            }
+
+        }
     }
 }
